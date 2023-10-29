6e1797b739e7bc5898aea8750843a7d39ff53f22
amalmathew07 <amalvm001@gmail.com>
2023-10-28 23:45:25 -0400

    Initial commit of the front end app

---
+++ log-search-ui/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  return (
+    <div>
+      TEST
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
+export default App;
